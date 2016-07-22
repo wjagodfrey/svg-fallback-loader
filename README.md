7bcc6bba999b3320fba68689190ec64d3ebaa0c3
Wilfred Godfrey <w.j.godfrey@gmail.com>
2016-07-22 15:15:40 +1200

    Add loader.

---
+++ README.md
@@ -0,0 +1,96 @@
+# svg-fallback-loader
+
+### IN DEVELOPMENT
+
+would not recommend
+
+------
+
+
+
+### Configuration
+
+The loader can be configured (defaults shown);
+
+```
+svgFallbackLoader: {
+  output:{
+    base: __dirname,
+    retina: './retina/',
+    fallback: './fallback/',
+    svg: './svg/'
+  }
+}
+```
+
+The key of the configuration object can be modified with the following loader query, if it conflicts for any reason:
+
+```
+loader: 'svg-fallback-loader?configKey=somethingElse',
+```
+
+### Example
+
+```
+module.exports = {
+  //...
+    module: {
+        loaders: [
+          test: /\.svg$/,
+          loader: 'svg-fallback-loader',
+          include: /(icons)/ // not required, only if you want this to apply to specific svgs
+        ]
+    },
+    //...
+    svgFallbackLoader: {
+      output:{
+        base: path.join(__dirname, './public/icons/')
+        // icon kind (retina, svg etc) directories use defaults
+      }
+    }
+    //...
+};
+```
+
+Now, when requiring an svg with webpack, you will get back an object like this:
+
+```
+{
+  name: "icon-arrow-left",
+  retina: "/Users/wilfred.godfrey/www/project_name/public/icons/retina/icon-arrow-left.png",
+  fallback: "/Users/wilfred.godfrey/www/project_name/public/icons/png/icon-arrow-left.png",
+  svg: "/Users/wilfred.godfrey/www/project_name/public/icons/svg/icon-arrow-left.svg"
+}
+```
+
+Clearly not useful in all situations, but useful nonetheless.
+
+------
+
+I am using it like so:
+
+```
+// icons.js
+
+'use strict';
+
+module.exports = {};
+
+requireAll(require.context('./icons/', true, /\.svg$/));
+
+function requireAll(r) {
+  r.keys().forEach((key) => {
+    let icon = r(key);
+    module.exports[icon.name] = icon;
+  });
+}
+```
+
+This module returns an object of icon definitions, with icon names as keys.
+
+This is then passed to a react component that handles which injects icons by name, and injects the correct icon src for the user's browser.
+
+### TODO
+
+* Add tests
+* Prevent icon name/path collisions
--- index.js
+++ index.js
@@ -1,6 +1,81 @@
 'use strict';
 
+const fs          = require("fs-promise");
+const path        = require('path');
+const svg2png     = require('svg2png');
+const sizeOf      = require('image-size');
+const mkdirp      = require('mkdirp-promise')
+const loaderUtils = require('loader-utils');
+const deepAssign  = require('deep-assign');
+
 module.exports = function(source) {
-  console.log('svg2png-loader:', source);
-  return source;
+  this.cacheable(true);
+
+  let query = loaderUtils.parseQuery(this.query);
+  var configKey = query.configKey || 'svgFallbackLoader';
+
+  let config = {
+    output:{
+      base: this.options.output.path,
+      retina: './retina/',
+      fallback: './fallback/',
+      svg: './svg/'
+    }
+  };
+
+  // extend user configuration onto defaults
+  config = deepAssign({}, config, this.options[configKey]);
+
+  // require a base path
+  if (!config.output.base) {
+    throw new Error('No base path configured');
+  }
+
+  // calculate paths
+  let svgResourcePath  = this.resourcePath;
+  let svgName          = path.basename(svgResourcePath, path.extname(svgResourcePath));
+  let basePath         = config.output.base;
+  let retinaPath       = path.join(basePath, config.output.retina, `${svgName}.png`);
+  let fallbackPath     = path.join(basePath, config.output.fallback, `${svgName}.png`);
+  let svgPath          = path.join(basePath, config.output.svg, `${svgName}.svg`);
+
+  let svgSize = sizeOf(svgResourcePath);
+
+  console.log(svgName, svgSize);
+
+  // perform conversions
+  fs.readFile(svgResourcePath)
+    .then(mkdirp(path.dirname(retinaPath)))
+    .then(mkdirp(path.dirname(fallbackPath)))
+    .then(mkdirp(path.dirname(svgPath)))
+    .then((svgBuffer) => {
+      return Promise.all([
+        // retina conversion
+        svg2png(svgBuffer, {
+          width: (svgSize.width * 2),
+          height: (svgSize.height * 2)
+        })
+          .then(buffer => fs.writeFile(retinaPath, buffer)),
+        // fallback conversion
+        svg2png(svgBuffer, {
+          width: svgSize.width,
+          height: svgSize.height
+        })
+          .then(buffer => fs.writeFile(fallbackPath, buffer))
+      ]);
+    })
+    // copy svgs across
+    .then(fs.copy(svgResourcePath, svgPath))
+    .catch((e) => {
+      throw new Error(e);
+    });
+
+  let result = `module.exports = {
+    name: '${svgName}',
+    retina: '${retinaPath}',
+    fallback: '${fallbackPath}',
+    svg: '${svgPath}',
+  };`;
+
+  return result;
 };
